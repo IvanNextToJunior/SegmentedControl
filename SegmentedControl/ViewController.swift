ecfa4ca360ef14b80a608a83e13a6e5b23cf7705
IvanNextToJunior <58988299+IvanNextToJunior@users.noreply.github.com>
2025-03-04 16:35:53 +0300

    query segment title

--- SegmentedControl/ViewController.swift
+++ SegmentedControl/ViewController.swift
@@ -9,9 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var label: UILabel!
+    @IBAction func chooseSegmentedControl(_ sender: UISegmentedControl) {
+        
+        label.isHidden = false
+        
+        switch segmentedControl.selectedSegmentIndex {
+        
+        case 0:
+            label.text = "The \(segmentedControl.titleForSegment(at: segmentedControl.selectedSegmentIndex)!)"
+            label.textColor = .red
+       
+        case 1:
+            label.text = "The \(segmentedControl.titleForSegment(at: segmentedControl.selectedSegmentIndex)!)"
+            label.textColor = .blue
+       
+        case 2:
+            label.text = "The \(segmentedControl.titleForSegment(at: segmentedControl.selectedSegmentIndex)!)"
+            label.textColor = .yellow
+            
+        default:
+            print("Something went wrong")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        label.isHidden = true
+        label.font = label.font.withSize(35)
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        
+        segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: true)
+        
     }
 
 
